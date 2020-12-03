990fb2320bdf0bf1c38832f3ae8c8cba79a7ad69
XiongLilly <lxiong11@uncc.edu>
2020-12-02 21:13:32 -0500

    Update AmazonReviewAnalyzeFields.java

--- AmazonReviewAnalyzeFields.java
+++ AmazonReviewAnalyzeFields.java
@@ -18,6 +18,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.StringTokenizer; 
 import java.util.ArrayList;
 import java.util.Arrays; 
