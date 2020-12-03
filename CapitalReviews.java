391001ff814b3eed9407cecb2327149f96650dbc
XiongLilly <lxiong11@uncc.edu>
2020-12-02 21:58:43 -0500

    Update CapitalReviews.java

--- CapitalReviews.java
+++ CapitalReviews.java
@@ -25,13 +25,13 @@ import java.util.Arrays;
  * * This Map-Reduce code will go through every Amazon review in rfox12:reviews It will then output
  * data on the top-level JSON keys
  */
-public class my_amazon_mapreduce extends Configured implements Tool {
+public class CapitalReviews extends Configured implements Tool {
   // Just used for logging
-  protected static final Logger LOG = LoggerFactory.getLogger(my_amazon_mapreduce.class);
+  protected static final Logger LOG = LoggerFactory.getLogger(CapitalReviews.class);
 
   // This is the execution entry point for Java programs
   public static void main(String[] args) throws Exception {
-    int res = ToolRunner.run(HBaseConfiguration.create(), new my_amazon_mapreduce(), args);
+    int res = ToolRunner.run(HBaseConfiguration.create(), new CapitalReviews(), args);
     System.exit(res);
   }
 
@@ -42,8 +42,8 @@ public class my_amazon_mapreduce extends Configured implements Tool {
     }
 
     // Now we create and configure a map-reduce "job"
-    Job job = Job.getInstance(getConf(), "my_amazon_mapreduce");
-    job.setJarByClass(my_amazon_mapreduce.class);
+    Job job = Job.getInstance(getConf(), "CapitalReviews");
+    job.setJarByClass(CapitalReviews.class);
 
     // By default we are going to can every row in the table
     Scan scan = new Scan();
@@ -90,7 +90,7 @@ public class my_amazon_mapreduce extends Configured implements Tool {
     @Override
     protected void setup(Context context) {
       parser = new JsonParser();
-      rowsProcessed = context.getCounter("my_amazon_mapreduce", "Rows Processed");
+      rowsProcessed = context.getCounter("CapitalReviews", "Rows Processed");
     }
 
     // This "map" method is called with every row scanned.
